90a1951822c6f344a8d119cba4c20f398497d5b7
sarthakjain1714 <sarthakjain1714@gmail.com>
2025-05-01 01:04:13 +0530

    solution 3

---
+++ loops/Loops_q3.java
@@ -0,0 +1,11 @@
+//Question3- print sum of number till 100
+public class Loops_q3 {
+    public static void main(String[] args) {
+        System.out.println("Sum of numbers till 100");
+        int sum = 0;
+        for (int i = 1; i <=100 ; i++) {
+            sum += i;
+        }
+        System.out.println("The sum is " +sum);
+    }
+}
