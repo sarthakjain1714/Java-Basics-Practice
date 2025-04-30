a6b88038f0965f3975946dd1216badb714cd1043
sarthakjain1714 <sarthakjain1714@gmail.com>
2025-04-30 17:18:40 +0530

    Added Question6

---
+++ InputAndOutput/Question6.java
@@ -0,0 +1,17 @@
+//Question6 - Swap two number using third variable
+import java.util.Scanner;
+
+public class Question6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first Number");
+        int a = input.nextInt();
+        System.out.println("Enter the Second Number");
+        int b = input.nextInt();
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("The first number is " +a);
+        System.out.println("The second number is " +b);
+    }
+}
