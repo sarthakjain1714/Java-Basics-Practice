b8625265a2d78255cf7b1f751775a8a116a052b3
sarthakjain1714 <sarthakjain1714@gmail.com>
2025-05-01 00:52:18 +0530

    solution 1

---
+++ loops/Loops_q1.java
@@ -0,0 +1,9 @@
+//Question1 - Print number from 1 to 20
+import java.util.Scanner;
+public class Loops_q1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+    }
+}
