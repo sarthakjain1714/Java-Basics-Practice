dbddcb8c75b1bb4d5097d769d2e495c527e35601
sarthakjain1714 <sarthakjain1714@gmail.com>
2025-04-30 14:52:46 +0530

    change

--- InputAndOutput/Question1.java
+++ InputAndOutput/Question1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int a = 10;
+        int a = input.nextInt();
         int b =20;
         int c = a+b;
         System.out.println("Sum is " +c);
