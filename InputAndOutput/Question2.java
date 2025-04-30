d6ea62d827f4faf83d36e5bcd21cb28eb0099b87
sarthakjain1714 <sarthakjain1714@gmail.com>
2025-04-30 15:00:52 +0530

    Question2

---
+++ InputAndOutput/Question2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter number 1");
+        int a = input.nextInt();
+        System.out.println("Enter Second Number");
+        int b = input.nextInt();
+        int c = a+b;
+        System.out.println("the sum is " +c);
+
+    }
+}
