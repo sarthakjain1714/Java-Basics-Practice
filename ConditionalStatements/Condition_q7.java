85e70d8f1279b9678893e8522ed39ba2cf7cacf9
sarthakjain1714 <sarthakjain1714@gmail.com>
2025-05-01 00:00:50 +0530

    Solution7

---
+++ ConditionalStatements/Condition_q7.java
@@ -0,0 +1,35 @@
+//Question7 - Write a program to take input as number and display weekend days
+import java.util.Scanner;
+
+public class Condition_q7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a = input.nextInt();
+        switch (a){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Enter a valid number");
+        }
+    }
+}
