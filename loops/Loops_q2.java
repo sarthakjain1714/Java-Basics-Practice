a0d93ede8b8974c2994027e64fd363c6fd7c111a
sarthakjain1714 <sarthakjain1714@gmail.com>
2025-05-01 00:52:01 +0530

    solution 2

---
+++ loops/Loops_q2.java
@@ -0,0 +1,10 @@
+//Question2 -  Print even number from 1 to 20
+public class Loops_q2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20  ; i++) {
+            if (i%2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
