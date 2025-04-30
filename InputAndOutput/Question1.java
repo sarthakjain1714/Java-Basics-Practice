f1e51990a8f3e715b0a69e3082f30f7fc0eaf757
sarthakjain1714 <sarthakjain1714@gmail.com>
2025-04-30 14:33:45 +0530

    question1

---
+++ InputAndOutput/Question1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Question1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = 10;
+        int b =20;
+        int c = a+b;
+        System.out.println("Sum is " +c);
+    }
+}
