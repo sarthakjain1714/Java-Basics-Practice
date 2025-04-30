597cdc6e599fce22d7d202ce5347ccd904c7db40
sarthakjain1714 <sarthakjain1714@gmail.com>
2025-04-30 22:02:02 +0530

    Solution1

--- ConditionalStatements/Condition_q1.java
+++ ConditionalStatements/Condition_q1.java
@@ -1,6 +1,7 @@
 //Question1- Take an integer as input and check whether it's even or odd.
 import java.util.Scanner;
 public class Condition_q1 {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the number");
