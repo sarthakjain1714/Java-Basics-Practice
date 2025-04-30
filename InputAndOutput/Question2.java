32f353966d40e1b7b01c9ca2c3284330b3b081c8
sarthakjain1714 <sarthakjain1714@gmail.com>
2025-04-30 15:37:48 +0530

    The correct Structure

--- InputAndOutput/Question2.java
+++ InputAndOutput/Question2.java
@@ -1,14 +1,18 @@
+//Question 2 - Take a number and print whether it is positive, Negative or zero
 import java.util.Scanner;
 
-public class Question2 {
+public class Question2{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("enter number 1");
+        System.out.println("Enter the number to check");
         int a = input.nextInt();
-        System.out.println("Enter Second Number");
-        int b = input.nextInt();
-        int c = a+b;
-        System.out.println("the sum is " +c);
-
+        if (a>0){
+            System.out.println("The number is Positive");
+        } else if (a<0) {
+            System.out.println("The number is Negative");
+        }
+        else {
+            System.out.println("The number is Zero");
+        }
     }
 }
