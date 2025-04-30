37ab740776eb27304d17f087e2ec40b90f5d621c
sarthakjain1714 <sarthakjain1714@gmail.com>
2025-04-30 15:37:01 +0530

    No major change

--- InputAndOutput/Question1.java
+++ InputAndOutput/Question1.java
@@ -1,10 +1,15 @@
+//Question1 - Take two number and print their sum
+
 import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("enter number 1");
         int a = input.nextInt();
-        int b =20;
+        System.out.println("Enter Second Number");
+        int b = input.nextInt();
         int c = a+b;
-        System.out.println("Sum is " +c);
+        System.out.println("the sum is " +c);
+
     }
 }
